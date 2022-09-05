b3cc1df2d6cc6ca65ce6cfdd73209a9466d014df
Rainaldo Dijkman <61870393+Asociateone@users.noreply.github.com>
2022-09-05 21:20:43 +0200

    updated store can now give forbidden
    A user cannot change another users list

--- app/Http/Controllers/RetroListsController.php
+++ app/Http/Controllers/RetroListsController.php
@@ -4,16 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RetroListRequest;
 use App\Http\Resources\RetroListResource;
+use App\Models\RetroLists;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class RetroListsController extends Controller
 {
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         return RetroListResource::collection(auth()->user()->retroList()->get());
     }
 
-    public function store(RetroListRequest $request)
+    public function store(RetroListRequest $request): RetroListResource
     {
         return RetroListResource::make(auth()->user()->retroList()->create($request->validated()));
+    }
+
+    public function update(RetroListRequest $request, RetroLists $list)
+    {
+        if(auth()->user()->id === $list->user_id){
+            $list->update($request->validated());
+        } else {
+            abort(403,'you are not allowed to do this!');
+        }
+        
+        return RetroListResource::make($list);
     }
 }
