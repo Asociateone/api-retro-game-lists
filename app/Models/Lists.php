3ebcb78e833e6e12a4f7fb5b065a754a2412db4b
Rainaldo Dijkman <61870393+Asociateone@users.noreply.github.com>
2022-09-07 20:46:47 +0200

    Added migrations - added new controller - added new route - added new model

--- app/Models/Lists.php
+++ app/Models/Lists.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Lists extends Model
@@ -21,5 +22,10 @@ class Lists extends Model
     public function users(): hasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function games(): HasMany
+    {
+        return $this->hasMany(Game::class);
     }
 }
